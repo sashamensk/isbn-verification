b2f2cf7b879e0262799572e235e6f9b10d0ac0f5
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-10-13 05:57:59 +0000

    Upload New File

---
+++ StringVerification/IsbnVerifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringVerification
+{
+    public static class IsbnVerifier
+    {
+        /// <summary>
+        /// Verifies if the string representation of number is a valid ISBN-10 identification number of book.
+        /// </summary>
+        /// <param name="number">The string representation of book's number.</param>
+        /// <returns>true if number is a valid ISBN-10 identification number of book, false otherwise.</returns>
+        /// <exception cref="ArgumentException">Thrown if number is null or empty or whitespace.</exception>
+        public static bool IsValid(string number)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
